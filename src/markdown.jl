24ec353f4d4faea3c719a5a130db29e7e773e521
Rratic <Rratic@163.com>
2022-07-21 17:16:14 +0800

    fix:error when alt empty

--- src/markdown.jl
+++ src/markdown.jl
@@ -155,10 +155,8 @@ function mkhtml(node::CommonMark.Node, link::CommonMark.Link, pss::PagesSetting)
 	return "<a href='$url' target='_blank'>$htm</a>"
 end
 function mkhtml(node::CommonMark.Node, img::CommonMark.Image, pss::PagesSetting)
-	alt = node.first_child.literal
-	if alt==""
-		alt=pss.default_alt
-	end
+	fc = node.first_child
+	alt = isdefined(fc, :literal) ? fc.literal : pss.default_alt
 	dest = img.destination
 	if startswith(dest, "/")
 		dest=joinpath(pss.server_prefix, dest[2:end])
