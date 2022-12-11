97c20472a93fc75399484c2f1a938eca27d2658b
Rratic <Rratic@163.com>
2022-12-11 13:14:54 +0800

    添加 random_word.js

---
+++ src/script/random_word.js
@@ -0,0 +1,18 @@
+for(let tag of $(".random-word")){
+	let id = tag.dataset["id"]
+	let data = magic(id)
+	let ind = floor(Math.random()*data.length)
+	let chosen = data[ind]
+	if(chosen instanceof String)chosen = {text: chosen}
+	let span = document.createElement("span")
+	span.innerText = data[ind].text
+	tag.appendChild(span)
+	delete chosen.text
+	for(let k of Object.keys(chosen)){
+		tag.appendChild(document.createElement("br"))
+		let box = document.createElement("div")
+		box.className = "box-hide"
+		box.innerHTML = `<button class='button-hide' onclick='unhide(event)'><span>${k}</span></button><div class='display-hide'>${chosen[k]}</div>`
+		tag.appendChild(box)
+	}
+}
