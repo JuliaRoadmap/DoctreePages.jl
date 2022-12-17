685df50ad9d0143664b84b6f218bd647839042db
Rratic <Rratic@users.noreply.github.com>
2022-12-17 07:28:52 +0000

    deploy: 1637b558102349305306ab34f6409028d7f1a5ef

--- extra/main.js
+++ extra/main.js
@@ -210,25 +210,26 @@ for(let i of $(".select-is")){
 }
 for(let tag of $(".random-word")){
 	let id = tag.dataset["id"]
-	let data
-	fetch(`${tURL}/extra/data_random_word/${id}.json`).then(function(response){
+	fetch(`${tURL}/extra/data_random_word/${id}.json`)
+	.then(response => {
+		if (!response.ok)throw new Error("HTTP error " + response.status);
 		return response.json()
-	}).then(function(x){
-		data = x
 	})
-	let ind = Math.floor(Math.random()*data.length)
-	let chosen = data[ind]
-	let span = document.createElement("span")
-	span.innerText = data[ind].text
-	tag.appendChild(span)
-	delete chosen.text
-	for(let k of Object.keys(chosen)){
+	.then(data => {
+		let ind = Math.floor(Math.random()*data.length)
+		let chosen = data[ind]
+		let span = document.createElement("span")
+		span.innerText = data[ind].text
+		tag.appendChild(span)
 		tag.appendChild(document.createElement("br"))
-		let box = document.createElement("div")
-		box.className = "box-hide"
-		box.innerHTML = `<button class='button-hide' onclick='unhide(event)'><span>${k}</span></button><div class='display-hide'>${chosen[k]}</div>`
-		tag.appendChild(box)
-	}
+		delete chosen.text
+		for(let k of Object.keys(chosen)){
+			let box = document.createElement("div")
+			box.className = "box-hide"
+			box.innerHTML = `<button class='button-hide' onclick='unhide(event)'><span>${k}</span></button><div class='display-hide'>${chosen[k]}</div>`
+			tag.appendChild(box)
+		}
+	})
 }
 	})
 })
